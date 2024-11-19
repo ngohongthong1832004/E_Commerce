a80eaedb1e2ffed106aad050f60f3ed5dffb0ec9
HienHarmony <21041211.hien@student.iuh.edu.vn>
2024-11-20 01:00:31 +0700

    "update"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# Thiết lập biến môi trường cơ bản
-export JAVA_HOME=${JAVA_HOME:-/opt/jdk}
-export HADOOP_HOME=${HADOOP_HOME:-/opt/hadoop}
+# Thêm Hadoop vào PATH
 export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH
-export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server:$LD_LIBRARY_PATH
-
-# Kiểm tra biến môi trường
-echo "JAVA_HOME hiện tại: $JAVA_HOME"
-echo "HADOOP_HOME hiện tại: $HADOOP_HOME"
-echo "PATH hiện tại: $PATH"
-echo "LD_LIBRARY_PATH hiện tại: $LD_LIBRARY_PATH"
 
 # Kiểm tra lệnh hdfs
-which hdfs || { echo "Lệnh hdfs không được tìm thấy trong PATH. Kiểm tra HADOOP_HOME."; exit 1; }
+echo "PATH hiện tại: $PATH"
+which hdfs || echo "Lệnh hdfs không được tìm thấy trong PATH."
 
 # Kiểm tra vai trò của container
 if [[ "$HOSTNAME" == "namenode" ]]; then
@@ -22,7 +14,6 @@ if [[ "$HOSTNAME" == "namenode" ]]; then
     mkdir -p /opt/hdfs/namenode /opt/hadoop/tiki_data
     chown -R root:root /opt/hdfs
 
-    # Kiểm tra xem NameNode đã được format chưa
     if [ ! -d "/opt/hdfs/namenode/current" ]; then
         echo "Formatting NameNode..."
         hdfs namenode -format -force -nonInteractive
@@ -35,8 +26,10 @@ if [[ "$HOSTNAME" == "namenode" ]]; then
 else
     echo "Starting DataNode setup..."
     mkdir -p /opt/hdfs/datanode
-    chown -R root:root /opt/hdfs
 
+    chown -R root:root /opt/hdfs
     echo "Starting DataNode..."
     exec hdfs datanode
 fi
+
+
